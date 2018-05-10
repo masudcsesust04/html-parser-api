5486106f9b6772ff17519df83db34b7ecce797d5
Masud Rana <masudcsesust04@gmail.com>
2018-05-11 01:21:38 +0600

    Added parse controller action and routes to parse html content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,5 @@
-class ContentsController < ApplicationController
-
-
-
+class ContentsController < JSONAPI::ResourceController
+  def parse
+    render json: { success: true }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  jsonapi_resources :contents
+  jsonapi_resources :contents do
+    get :parse, on: :collection
+  end
 end
